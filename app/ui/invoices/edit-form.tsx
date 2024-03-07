f8c7cd0361a6049a143e77fcd93f14c7d8bd821a
rahman-a <ahm.abdrahman@gamil.com>
2024-03-07 18:42:42 +0200

    nextjs learn pt2

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -8,7 +8,7 @@ import {
   UserCircleIcon,
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
-import { Button } from '@/app/_ui/button';
+import { Button } from '@/app/ui/button';
 
 export default function EditInvoiceForm({
   invoice,
